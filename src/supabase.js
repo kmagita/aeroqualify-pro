bbd2a45a06bf6531b82f166734de44853c21bee6
kmagita <kmagita@gmail.com>
2026-03-06 10:00:36 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -3,9 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL  = 'https://lsgawxzpilototfsummm.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL  = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON = 'YOUR_ANON_PUBLIC_KEY';
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON, {
   auth: { persistSession: true, autoRefreshToken: true },
   realtime: { params: { eventsPerSecond: 10 } },
